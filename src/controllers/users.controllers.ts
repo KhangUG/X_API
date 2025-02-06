4b2fa4d90a3beea91955d147363e0191e0e612e3
khangug <giapvankhang6789@gmail.com>
2025-02-06 22:54:09 +0700

    register

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,15 +1,34 @@
 import { Request, Response } from 'express'
+import User from '~/models/schemas/User.schema'
+import databaseService from '~/services/database.services'
 
-
-
-export const userController = async (req: Request, res: Response): Promise<void> => {
-
+export const loginController = async (req: Request, res: Response) => {
   const { email, password } = req.body
   if (email == 'giapvankhang6789@gmail.com' && password == '123456') {
-    res.status(200).json({ message: 'Success' });
-  }else{
-  res.status(400).json({
-    message: 'email or password is incorrect'
-  })
+    res.status(200).json({ message: 'Success' })
+  } else {
+    res.status(400).json({
+      message: 'email or password is incorrect'
+    })
   }
-};
+}
+
+export const registerController = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body
+  try {
+    const result = await databaseService.users.insertOne(
+      new User({
+        email,
+        password
+      })
+    )
+    
+    res.status(400).json({
+      message: 'Register success'
+    })
+  } catch (error) {
+    res.status(400).json({
+      message: 'Register fail'
+    })
+  }
+}
--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,28 +1,33 @@
-import { MongoClient } from 'mongodb';
-import {config} from 'dotenv';
-config();
+import { MongoClient, Db, Collection } from 'mongodb'
+import User from '../models/schemas/User.schema'
+import { config } from 'dotenv'
+config()
 
 // URI kết nối tới MongoDB
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@x-api.xy7gt.mongodb.net/?retryWrites=true&w=majority&appName=X-API`
 
-
-class DatabaseServices { 
-  private client: MongoClient;
-  constructor(){
-    this.client = new MongoClient(uri);
+class DatabaseServices {
+  private client: MongoClient
+  private db: Db
+  constructor() {
+    this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
   }
 
-  async connect(){
+  async connect() {
     try {
-      await this.client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      await this.client.close();
+      await this.db.command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } catch (error) {
+      console.log('There was an error connecting to MongoDB. Check your connection string. Error: ', error)
     }
+  }
+  get users(): Collection<User> {
+    return this.db.collection(process.env.DB_USER_COLLECTION as string)
   }
 }
 
 // Tạo object từ class DatabaseServices
-const databaseService = new DatabaseServices();
+const databaseService = new DatabaseServices()
 
-export default databaseService;
+export default databaseService
