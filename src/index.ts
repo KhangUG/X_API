4252d0278b8cc415fe59685585c16f1a27e3c814
khangug <giapvankhang6789@gmail.com>
2025-02-09 22:21:20 +0700

    tao wraprequestHandler

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, {Request, Response, NextFunction } from 'express'
 import userRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 const app = express()
@@ -8,6 +8,9 @@ app.use(express.json())
 app.use('/users', userRouter)
 
 databaseService.connect()
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  res.status(400).json({ error: err.message })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
