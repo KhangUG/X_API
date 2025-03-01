bff75c4e42ca29ccde887b8e467940acbd18e8e6
khangug <giapvankhang6789@gmail.com>
2025-03-01 22:11:40 +0700

    Filter upload ảnh

---
+++ src/utils/file.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Request, Response } from 'express'
+import formidable from 'formidable'
+import path from 'path'
+
+export const handleUploadSingleImage = async (req: Request) => {
+  const form = formidable({
+    uploadDir: path.resolve('uploads'),
+    maxFiles: 1,
+    keepExtensions: true,
+    maxFileSize: 300 * 1024 * 1024,
+    filter: function ({ name, originalFilename, mimetype }) {
+      const valid = name == 'image' && Boolean(mimetype?.includes('image/'))
+      if (!valid) {
+        form.emit('error' as any, new Error('Invalid file type') as any)
+      }
+      return valid
+    }
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      // eslint-disable-next-line no-extra-boolean-cast
+      if(!Boolean(files.image)) {
+        return reject(new Error('File is empty'))
+      }
+      resolve(files)
+    })
+  })
+}
