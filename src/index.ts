b9443d48242074c5f016125a66257029db5681fe
khangug <giapvankhang6789@gmail.com>
2025-03-01 21:22:58 +0700

    Upload file bằng formidable

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import userRouter from './routes/users.routes'
 import databaseService from './services/database.services'
 import { pick } from 'lodash'
 import { defaultErrorHandler } from './middlewares/error.middlewares'
+import mediasRouter from './routes/medias.routes'
 databaseService.connect()
 const app = express()
 const port = 4000
 
 app.use(express.json())
 app.use('/users', userRouter)
+app.use('/medias', mediasRouter)
 
 app.use(defaultErrorHandler)
 
