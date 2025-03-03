ffec5021010474bba9e859cd182c576d950ee4f2
khangug <giapvankhang6789@gmail.com>
2025-03-03 23:59:14 +0700

    Upload Video

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,24 +1,25 @@
 import { NextFunction, Request, Response } from 'express'
 import formidable, { File } from 'formidable'
-import path from 'path'
-import { UPLOAD_TEMP_DIR } from '~/constants/dir'
+import { UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_DIR, UPLOAD_VIDEO_TEMP_DIR } from '~/constants/dir'
 import fs from 'fs'
+
 export const initFolder = () => {
-  const uploadFolderPath = path.resolve('uploads')
-  if (!fs.existsSync(UPLOAD_TEMP_DIR)) {
-    fs.mkdirSync(UPLOAD_TEMP_DIR, {
-      recursive: true // mục đích là để tạo folder nested
-    })
-  }
+  ;[UPLOAD_IMAGE_TEMP_DIR, UPLOAD_VIDEO_TEMP_DIR].forEach((dir) => {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, {
+        recursive: true // mục đích là để tạo folder nested
+      })
+    }
+  })
 }
 
 export const handleUploadImage = async (req: Request) => {
   const form = formidable({
-    uploadDir: UPLOAD_TEMP_DIR,
+    uploadDir: UPLOAD_IMAGE_TEMP_DIR,
     maxFiles: 4,
     keepExtensions: true,
-    maxFileSize: 300 * 1024 * 1024,
-    maxTotalFileSize: 300 * 1024 * 1024 * 4,
+    maxFileSize: 500 * 1024,
+    maxTotalFileSize: 500 * 1024 * 4,
     filter: function ({ name, originalFilename, mimetype }) {
       const valid = name == 'image' && Boolean(mimetype?.includes('image/'))
       if (!valid) {
@@ -40,9 +41,49 @@ export const handleUploadImage = async (req: Request) => {
     })
   })
 }
+export const handleUploadVideo = async (req: Request) => {
+  const form = formidable({
+    uploadDir: UPLOAD_VIDEO_DIR,
+    maxFiles: 1,
+    // keepExtensions: true,
+    maxFileSize: 50 * 1024 * 1024,
+    filter: function ({ name, originalFilename, mimetype }) {
+      return true
+      // const valid = name == 'image' && Boolean(mimetype?.includes('image/'))
+      // if (!valid) {
+      //   form.emit('error' as any, new Error('Invalid file type') as any)
+      // }
+      // return valid
+    }
+  })
+  return new Promise<File[]>((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return reject(err)
+      }
+      // eslint-disable-next-line no-extra-boolean-cast
+      if (!Boolean(files.video)) {
+        return reject(new Error('File is empty'))
+      }
+      
+      const videos = files.video as File[]
+      videos.forEach((video) => {
+        const ext  = getExtension(video.originalFilename as string)
+        fs.renameSync(video.filepath, video.filepath + '.' + ext)
+        video.newFilename = video.newFilename + '.' + ext
+      })
+      resolve(files.video as File[])
+    })
+  })
+}
 
 export const getNameFromFullName = (fullname: string) => {
   const namearr = fullname.split('.')
   namearr.pop()
   return namearr.join('')
 }
+
+export const getExtension = (fullname: string) => {
+  const namearr = fullname.split('.')
+  return namearr[namearr.length - 1]
+} 
