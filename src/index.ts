ad79d3c5dbdce49d17f4da822e8e322874cb1d69
khangug <giapvankhang6789@gmail.com>
2025-03-05 06:10:41 +0700

    fix routes tweet

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import mediasRouter from './routes/medias.routes'
 import { initFolder } from './utils/file'
 import { config } from 'dotenv'
 import { UPLOAD_IMAGE_DIR, UPLOAD_VIDEO_DIR } from './constants/dir'
-import staticRouter from './routes/static.routes'
+import staticRouter from '~/routes/static.routes'
+import tweetsRouter from '~/routes/tweets.routes'
 config()
 
 databaseService.connect()
@@ -20,8 +21,10 @@ initFolder()
 app.use(express.json())
 app.use('/users', userRouter)
 app.use('/medias', mediasRouter)
+app.use('/tweets', tweetsRouter)
 app.use('/static', staticRouter)
 app.use('/static/video', express.static(UPLOAD_VIDEO_DIR))
+
 
 app.use(defaultErrorHandler)
 
