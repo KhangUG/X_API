a4a83a8019314431bc87a429a67900e7ec9eec45
khangug <giapvankhang6789@gmail.com>
2025-03-07 05:15:49 +0700

    login nhanh tren client bang accesstoekn

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,29 @@
 import { RouterProvider } from 'react-router-dom'
 import './App.css'
 import routes from './router'
+import { useEffect } from 'react'
 
+import axios from 'axios'
 
 function App() {
-  return (
-    <RouterProvider router={routes} />
-  )
+  useEffect(() => {
+    const controller = new AbortController()
+    axios
+      .get('/users/me', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('access_token')}`
+        },
+        baseURL: import.meta.env.VITE_API_URL,
+        signal: controller.signal
+      })
+      .then((res) => {
+        localStorage.setItem('profile', JSON.stringify(res.data.result))
+      })
+    return () => {
+      controller.abort()
+    }
+  }, [])
+  return <RouterProvider router={routes} />
 }
 
 export default App
--- Client/src/Home.jsx
+++ Client/src/Home.jsx
@@ -25,6 +25,7 @@ const googleOAuthUrl = getGoogleAuthUrl()
 
 export default function Home() {
   const isAuthenticated = Boolean(localStorage.getItem('access_token'))
+  const profile = JSON.parse(localStorage.getItem('profile')) || {}
   const logout = () => {
     localStorage.removeItem('access_token')
     localStorage.removeItem('refresh_token')
