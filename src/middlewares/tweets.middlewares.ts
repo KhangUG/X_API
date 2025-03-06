4018b7a7f5fbb13d31fbda3f3a6068f78ae3e754
khangug <giapvankhang6789@gmail.com>
2025-03-06 18:32:10 +0700

    tweet validatate

--- src/middlewares/tweets.middlewares.ts
+++ src/middlewares/tweets.middlewares.ts
@@ -106,3 +106,32 @@ export const creatTweetValidator = validate(
     }
   })
 )
+export const tweetIdValidator = validate(
+  checkSchema(
+    {
+      tweet_id: {
+        custom: {
+          options: async (value, { req }) => {
+            if (!ObjectId.isValid(value)) {
+              throw new ErrorWithStatus({
+                status: HTTP_STATUS.BAD_REQUEST,
+                message: TWEETS_MESSAGES.INVALID_TWEET_ID
+              })
+            }
+            const tweet = await databaseService.tweets.findOne({
+              _id: new ObjectId(value)
+            })
+            if (!tweet) {
+              throw new ErrorWithStatus({
+                status: HTTP_STATUS.NOT_FOUND,
+                message: TWEETS_MESSAGES.TWEET_NOT_FOUND
+              })
+            }
+            return true
+          }
+        }
+      }
+    },
+    ['params', 'body']
+  )
+)
