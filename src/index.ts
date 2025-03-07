c82cde3b7a21f71bf9dc04ef2ce86766cb54597f
khangug <giapvankhang6789@gmail.com>
2025-03-07 16:14:50 +0700

    Swagger cho Express.js REST API

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,26 @@ import bookmarksRouter from './routes/bookmarks.routes'
 import likesRouter from './routes/likes.routes'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import YAML from 'yaml'
 
+// import fs from 'fs'
+// import path from 'path'
+import swaggerUi from 'swagger-ui-express'
+import swaggerJsdoc from 'swagger-jsdoc'
+// const file = fs.readFileSync(path.resolve('twitter-swagger.yaml'), 'utf8')
+// const swaggerDocument = YAML.parse(file)
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'X clone (Twitter API)',
+      version: '1.0.0'
+    }
+  },
+  apis: ['./openapi/*.yaml'] // files containing annotations as above
+}
+const openapiSpecification = swaggerJsdoc(options)
 
 config()
 
@@ -29,6 +48,8 @@ initFolder()
 
 app.use(express.json())
 app.use('/users', userRouter)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification))
 app.use('/medias', mediasRouter)
 app.use('/tweets', tweetsRouter)
 app.use('/bookmarks', bookmarksRouter)
--- src/models/requests/User.requests.ts
+++ src/models/requests/User.requests.ts
@@ -2,6 +2,90 @@ import { JwtPayload } from 'jsonwebtoken'
 import { TokenType, UserVerifyStatus } from '~/constants/enums'
 import { ParamsDictionary } from 'express-serve-static-core'
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     LoginBody:
+ *       type: object
+ *       properties:
+ *         email:
+ *           type: string
+ *           example: khang9@gmail.com
+ *         password:
+ *           type: string
+ *           example: Khang123@
+ *     SuccessAuthentication:
+ *       type: object
+ *       properties:
+ *         access_token:
+ *           type: string
+ *           example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjdjMWNlMmEzOGRlOGIzNWVkMzkxOTdmIiwidG9rZW5fdHlwZSI6MCwidmVyaWZ5IjoxLCJpYXQiOjE3NDEzMzc2NTksImV4cCI6MTc0MTMzODU1OX0.NewLxVeQMbGEbfvmYZ52VKrsZn8nrMCuplvWw0LFiyI
+ *         refresh_token:
+ *           type: string
+ *           example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNjdjMWNlMmEzOGRlOGIzNWVkMzkxOTdmIiwidG9rZW5fdHlwZSI6MSwidmVyaWZ5IjoxLCJpYXQiOjE3NDEzMzc2NTksImV4cCI6MTc0OTk3NzY1OX0.RYCaPTpCSnCF_AwQnqo84wHYhPeHo_YPrQEtXGIL8-U
+ *     User:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           format: MongoId
+ *           example: 64be0ad2e43d2464394feedb
+ *         name:
+ *           type: string
+ *           example: John Doe
+ *         email:
+ *           type: string
+ *           example: johndoe@example.com
+ *         date_of_birth:
+ *           type: string
+ *           format: ISO8601
+ *           example: 2023-06-08T10:17:31.096Z
+ *         created_at:
+ *           type: string
+ *           format: ISO8601
+ *           example: 2023-03-08T12:00:00Z
+ *         updated_at:
+ *           type: string
+ *           format: ISO8601
+ *           example: 2023-03-08T12:00:00Z
+ *         verify:
+ *           $ref: '#/components/schemas/UserVerifyStatus'
+ *         twitter_circle:
+ *           type: array
+ *           items:
+ *             type: string
+ *             format: MongoId
+ *           example: ['64be0ad2e43d2464394feedb', '64be0ad2e43d2464394feedc']
+ *         bio:
+ *           type: string
+ *           example: 'This is my bio.'
+ *         location:
+ *           type: string
+ *           example: 'San Francisco, CA'
+ *         website:
+ *           type: string
+ *           example: 'www.example.com'
+ *         username:
+ *           type: string
+ *           example: 'johndoe'
+ *         avatar:
+ *           type: string
+ *           example: 'http:localhost:4000/images/avatars/johndoe.jpg'
+ *         cover_photo:
+ *           type: string
+ *           example: 'http:localhost:4000/images/avatars/johndoe.jpg'
+ *     UserVerifyStatus:
+ *       type: number
+ *       enum: [Unverified, Verified, Banned]
+ *       example: 1
+ *   securitySchemes:
+ *     BearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
 export interface RegisterReqBody {
   name: string
   email: string
