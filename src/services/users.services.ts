e3f22626c0aa6e8acfbbd528bc6e5ce8036ecb8a
khangug <giapvankhang6789@gmail.com>
2025-03-07 14:00:31 +0700

    Gửi email bằng template html có sẵn

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -12,7 +12,7 @@ import { ErrorWithStatus } from '~/models/Errors'
 import HTTP_STATUS from '~/constants/httpStatus'
 import Follower from '~/models/schemas/Follower.schema'
 import axios from 'axios'
-import { sendVerifyEmail } from '~/utils/email'
+import { sendForgotPasswordEmail, sendVerifyRegisterEmail } from '~/utils/email'
 config()
 
 class UsersService {
@@ -132,14 +132,7 @@ class UsersService {
     // 3. Client send request to server with email_verify_token
     // 4. Server verify email_verify_token
     // 5. Client receive access_token and refresh_token
-    await sendVerifyEmail(
-      payload.email,
-      'Verify your email',
-      `
-      <h1>Verify your email</h1>
-      <p>Click <a href="${process.env.CLIENT_URL}/verify-email?token=${email_verify_token}">here</a> to verify your email</p>
-      `
-    )
+    await sendVerifyRegisterEmail(payload.email, email_verify_token)
     return {
       access_token,
       refresh_token,
@@ -301,13 +294,13 @@ class UsersService {
     }
   }
 
-  async resendVerifyEmail(user_id: string) {
+  async resendVerifyEmail(user_id: string, email: string) {
     const email_verify_token = await this.signEmailVerifyToken({
       user_id,
       verify: UserVerifyStatus.Unverified
     })
     // Gỉa bộ gửi email
-    console.log('Rensend verify email: ', email_verify_token)
+    await sendVerifyRegisterEmail(email, email_verify_token)
 
     // Cập nhật lại giá trị email_verify_token trong document user
     await databaseService.users.updateOne(
@@ -325,7 +318,7 @@ class UsersService {
       message: USERS_MESSAGES.RESEND_VERIFY_EMAIL_SUCCESS
     }
   }
-  async forgotPassword({ user_id, verify }: { user_id: string; verify: UserVerifyStatus }) {
+  async forgotPassword({ user_id, verify, email }: { user_id: string; email: string; verify: UserVerifyStatus }) {
     const forgot_password_token = await this.signForgotPasswordToken({
       user_id,
       verify
@@ -339,7 +332,7 @@ class UsersService {
       }
     ])
     // Gửi email kèm đường link đến email người dùng: https://twitter.com/forgot-password?token=token
-    console.log('forgot_password_token: ', forgot_password_token)
+    await sendForgotPasswordEmail(email, forgot_password_token)
     return {
       message: USERS_MESSAGES.CHECK_EMAIL_TO_RESET_PASSWORD
     }
--- src/utils/email.ts
+++ src/utils/email.ts
@@ -2,7 +2,8 @@
 /* eslint-disable no-undef */
 import { SendEmailCommand, SESClient } from '@aws-sdk/client-ses'
 import { config } from 'dotenv'
-
+import fs from 'fs'
+import path from 'path'
 config()
 // Create SES service object.
 const sesClient = new SESClient({
@@ -12,6 +13,7 @@ const sesClient = new SESClient({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID as string
   }
 })
+const verifyEmailTemplate = fs.readFileSync(path.resolve('src/templates/verify-email.html'), 'utf8')
 
 const createSendEmailCommand = ({
   fromAddress,
@@ -53,7 +55,7 @@ const createSendEmailCommand = ({
   })
 }
 
-export const sendVerifyEmail = (toAddress: string, subject: string, body: string) => {
+const sendVerifyEmail = (toAddress: string, subject: string, body: string) => {
   const sendEmailCommand = createSendEmailCommand({
     fromAddress: process.env.SES_FROM_ADDRESS as string,
     toAddresses: toAddress,
@@ -61,4 +63,36 @@ export const sendVerifyEmail = (toAddress: string, subject: string, body: string
     subject
   })
   return sesClient.send(sendEmailCommand)
+}
+
+export const sendVerifyRegisterEmail = (
+  toAddress: string,
+  email_verify_token: string,
+  template: string = verifyEmailTemplate
+) => {
+  return sendVerifyEmail(
+    toAddress,
+    'Verify your email',
+    template
+      .replace('{{title}}', 'Please verify your email')
+      .replace('{{content}}', 'Click the button below to verify your email')
+      .replace('{{titleLink}}', 'Verify')
+      .replace('{{link}}', `${process.env.CLIENT_URL}/verify-email?token=${email_verify_token}`)
+  )
+}
+
+export const sendForgotPasswordEmail = (
+  toAddress: string,
+  forgot_password_token: string,
+  template: string = verifyEmailTemplate
+) => {
+  return sendVerifyEmail(
+    toAddress,
+    'Forgot Password',
+    template
+      .replace('{{title}}', 'You are receiving this email because you requested to reset your password')
+      .replace('{{content}}', 'Click the button below to reset your password')
+      .replace('{{titleLink}}', 'Reset Password')
+      .replace('{{link}}', `${process.env.CLIENT_URL}/reset-password?token=${forgot_password_token}`)
+  )
 }
